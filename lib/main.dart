f42ad9dd975f4ece754925bf75c56c2fe29128e9
hao88 <52927295@qq.com>
2023-12-16 20:36:36 +0800

    delete ffi

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:ptf/screens/login_page.dart';
 import 'package:ptf/screens/main_screen.dart';
 import 'package:ptf/theme/theme.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
